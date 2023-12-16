077c15bff0bc48b2f75ff45b8ab4226be2ce9ce4
André Steinberger <andre.steinberger@mac.com>
2023-12-16 21:53:38 +0100

    DBContext und Start mit Vue

---
+++ src/Einkaufszettel/Repository/EinkaufContext.cs
@@ -0,0 +1,19 @@
+using Microsoft.EntityFrameworkCore;
+using Einkaufszettel.Domain;
+
+namespace Einkaufszettel.Repository
+{
+    public class EinkaufContext : DbContext
+    {
+        public DbSet<Bedarf> Bedarfe { get; set; }
+
+        public DbSet<Haushalt> Haushalte { get; set; }
+
+        public DbSet<Produkt> Produkte { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlite("sqlite:///Einkaufszettel.db");
+        }
+    }
+}
